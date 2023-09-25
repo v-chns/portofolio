9d0bbff6b1068bd43f808cb36100a45884ddc4db
unknown <chansvanessa@gmail.com>
2023-09-25 18:49:52 +0700

    Hero Section

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,19 +1,45 @@
-import React from 'react'
-import Image from 'next/image'
+"use client";
+import React from 'react';
+import Image from 'next/image';
+import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
     return (
         <section>
-            <div className="grid grid-cols-1 lg:grid-cols-12">
+            <div className="grid grid-cols-1 sm:grid-cols-12">
                 {/* text */}
-                <div className="col-span-7 place-self-center">
-                    <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello, I&lsquo;m Vanessa</h1>
-                    <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
+                <div className="col-span-7 place-self-center text-center sm:text-left">
+                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-teal-700 via-cyan-600 to-indigo-700">
+                            Hello! I&lsquo;m Vanessa,
+                        </span>
+                        <br></br>
+                        a {" "}
+                        <TypeAnimation
+                            sequence={[
+                                'Student',
+                                1000,
+                                'Data Enthusiast',
+                                1000,
+                                'Web Developer',
+                                1000,
+                                'UI/UX Designer',
+                                1000
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            repeat={Infinity}
+                        />
+                    </h1>
+                    <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
                         Welcome, stay as long as you want;)
                     </p>
+
                     {/* buttons */}
                     <div>
-                        <button className="px-6 py-3 rounded-full bg-white hover:bg-slate-200 text-black">Download CV</button>
+                        <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-teal-700 via-cyan-600 to-indigo-700 hover:bg-slate-800 text-white mt-3">
+                            <span className="block bg-[#1f1f1f] hover:bg-slate-900 rounded-full px-5 py-2">Download CV</span>
+                        </button>
                     </div>
 
                 </div>
--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,10 @@ import HeroSection from './components/HeroSection'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#1f1f1f] container mx-auto px-12 py-4">
-      <HeroSection />
+    <main className="flex min-h-screen flex-col bg-[#1f1f1f]">
+      <div class="container mx-auto px-12 py-4">
+        <HeroSection />e
+      </div>
     </main>
   )
 }
