4dfe076f1faec16f08fef6b68ee2514c592f5978
unknown <chansvanessa@gmail.com>
2023-09-25 11:05:57 +0700

    Merge

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-// next.config.js
-module.exports = {
-    // Other configuration options...
-  
-    // Enable static HTML export
-    output: 'out',
-  };
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+   
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+   
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+   
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+  }
+   
+  module.exports = nextConfig
