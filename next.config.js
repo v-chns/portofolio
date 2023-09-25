5d32bf37e95ff609a1275fa0c020d6fd867e557c
unknown <chansvanessa@gmail.com>
2023-09-25 11:03:21 +0700

    Merge changes

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@
 module.exports = {
     // Other configuration options...
   
-    trailingSlash: true, // Use trailingSlash instead of exportTrailingSlash
-  
-    // Define your static paths using generateStaticParams
-    async generateStaticParams() {
-      // Define your static paths here
-      return [
-        {
-          route: '/',
-        },
-        // Add more paths as needed
-      ];
-    },
+    // Enable static HTML export
+    output: 'out',
   };
