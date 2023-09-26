8f774928c0ebbc23fbb8b8a5e56ec3d8ffd76558
unknown <chansvanessa@gmail.com>
2023-09-26 16:51:33 +0700

    Navbar update

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,30 +1,72 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import NavLink from './NavLink';
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import MenuOverlay from './MenuOverlay';
+
+const navLinks = [
+    {
+        title: "About Me",
+        path: "#aboutme"
+    },
+    {
+        title: "Projects",
+        path: "#projects"
+    },
+    {
+        title: "Contact me",
+        path: "#contactme"
+    }
+]
 
 function Navbar() {
-  return (
-    <nav>
-        <div className='flex flex-wrap items-center justify-between mx-auto p-8'>
-           {/* logo */}
-            <Link href={"/"}>
-                <Image
-                    src="https://v-chns.github.io/portofolio/images/logo.png"
-                    alt="logo"
-                    width={70}
-                    height={70}
-                    className="w-[80px] h-[80px] lg:w-[100px] lg:h-[100px] relative"
-                />
-            </Link>
-            {/* menu */}
-            <div className="menu hidden md:block md:w-auto" id="navbar">
-                <ul>
-                    <li><Link href={"#about"} className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white">About me</Link></li>    
-                </ul>   
+    const [navbarOpen, setNavbarOpen] = useState(false);
+
+    return (
+        <nav className="fixed top-0 left-0 right-0 z-10 bg-[#1f1f1f] bg-opacity-80 backdrop-blur-sm">
+            <div className='flex flex-wrap items-center justify-between mx-auto px-5 py-2'>
+                {/* logo */}
+                <Link href={"/"}>
+                    <Image
+                        src="https://v-chns.github.io/portofolio/images/logo.png"
+                        alt="logo"
+                        width={70}
+                        height={70}
+                        className="w-[40px] h-[40px] md:w-[60px] md:h-[60px] lg:w-[80px] lg:h-[80px] relative"
+                    />
+                </Link>
+                {/* hamburger menu */}
+                <div className=" mobile-menublock md:hidden">
+                    { !navbarOpen ? (
+                            <button onClick={()=> setNavbarOpen(true)} className="flex items-center px-3 py-2 border rounded border-slate-200 hover:text-white hover:border-white text-slate-200">
+                                <Bars3Icon className="h-5 w-5"/>
+                            </button>
+                        ) : (
+                            <button onClick={()=> setNavbarOpen(false)} className="flex items-center px-3 py-2 border rounded border-slate-200 hover:text-white hover:border-white text-slate-200">
+                                <XMarkIcon className="h-5 w-5"/>
+                            </button>
+                        )
+                    }
+                </div>
+
+                {/* menu */}
+                <div className="menu hidden md:block md:w-auto" id="navbar">
+                    <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+                        {
+                            navLinks.map((link, index) => (
+                                <li key={index}>
+                                    <NavLink href={link.path} title={link.title} />
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-  )
+            {navbarOpen ? <MenuOverlay links={navLinks}/> : null}
+        </nav>
+    )
 }
 
 export default Navbar
