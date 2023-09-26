e25afb76ee0d995c307e961bd7c0c449e3c5bb4d
unknown <chansvanessa@gmail.com>
2023-09-26 14:44:14 +0700

    Logo update

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,12 +1,27 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 function Navbar() {
   return (
     <nav>
-        <div>
-            <Link href={"/"}LOGO></Link>
-            <div className="menu"></div>
+        <div className='flex flex-wrap items-center justify-between mx-auto p-8'>
+           {/* logo */}
+            <Link href={"/"}>
+                <Image
+                    src="/images/logo.png"
+                    alt="logo"
+                    width={70}
+                    height={70}
+                    className="w-[80px] h-[80px] lg:w-[100px] lg:h-[100px] relative"
+                />
+            </Link>
+            {/* menu */}
+            <div className="menu hidden md:block md:w-auto" id="navbar">
+                <ul>
+                    <li><Link href={"#about"} className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white">About me</Link></li>    
+                </ul>   
+            </div>
         </div>
     </nav>
   )
